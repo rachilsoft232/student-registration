31b300fe621fec5e793a8fc19f35669a391300eb
rachilsoft232 <rachilpp999@gmail.com>
2024-08-10 12:55:19 +0530

    some error fixed

--- student_registration/sr/forms.py
+++ student_registration/sr/forms.py
@@ -10,3 +10,11 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = ['name','email', 'number', 'age', 'enrollment_date', 'course']
+
+    def __init__(self, *args, **kwargs):
+        super(StudentForm, self).__init__(*args, **kwargs)
+        self.fields['course'].queryset = Course.objects.filter(name__in=['Python', 'PHP', 'Java'])
+        self.fields['course'].queryset = Course.objects.filter(name__iexact='Python') | \
+                                  Course.objects.filter(name__iexact='PHP') | \
+                                  Course.objects.filter(name__iexact='Java')
+
